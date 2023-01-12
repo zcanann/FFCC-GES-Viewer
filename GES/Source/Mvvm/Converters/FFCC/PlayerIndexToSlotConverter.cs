90fab8ed11e91da3359dcb699266dd82a76feaa1
Zachary Canann <zahcarycanann@gmail.com>
2023-01-11 18:04:57 -0800

    WIP slot index display for gba views

---
+++ GES/Source/Mvvm/Converters/FFCC/PlayerIndexToSlotConverter.cs
@@ -0,0 +1,60 @@
+﻿namespace GES.Source.Mvvm.Converters
+{
+    using GES.Engine.Common;
+    using GES.Source.InventoryViewer;
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    /// <summary>
+    /// Converts an Int32 value to a hexedecimal value.
+    /// </summary>
+    public class PlayerIndexToSlotConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts an Int32 to a Hex string.
+        /// </summary>
+        /// <param name="value">Value to be converted.</param>
+        /// <param name="targetType">Type to convert to.</param>
+        /// <param name="parameter">Optional conversion parameter.</param>
+        /// <param name="culture">Globalization info.</param>
+        /// <returns>A hex string. If conversion cannot take place, returns null.</returns>
+        public Object Convert(Object value, Type targetType, Object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return String.Empty;
+            }
+
+            if (value is Int32)
+            {
+                return InventoryViewerViewModel.GetInstance().DisplayPlayerToSlotMap[(Int32)value];
+            }
+
+            return String.Empty;
+        }
+
+        /// <summary>
+        /// Hex string to an Int32.
+        /// </summary>
+        /// <param name="value">Value to be converted.</param>
+        /// <param name="targetType">Type to convert to.</param>
+        /// <param name="parameter">Optional conversion parameter.</param>
+        /// <param name="culture">Globalization info.</param>
+        /// <returns>An Int32. If conversion cannot take place, returns 0.</returns>
+        public Object ConvertBack(Object value, Type targetType, Object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                if (SyntaxChecker.CanParseHex(targetType, value.ToString()))
+                {
+                    return Conversions.ParseHexStringAsPrimitive(targetType, value.ToString());
+                }
+            }
+
+            return 0;
+        }
+    }
+    //// End class
+}
+//// End namespace
